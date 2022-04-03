ff06335621ad866a6a3defd7cf795fd1d811c832
Athul <65664185+Athul0491@users.noreply.github.com>
2022-04-03 23:38:18 +0530

    location selector

--- components/Map.js
+++ components/Map.js
@@ -5,14 +5,16 @@ const style = {
   wrapper: `flex-1 h-full w-full`,
 };
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-
+mapboxgl.accessToken =
+  process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ||
+  "pk.eyJ1IjoiYXRodWwwNDkxIiwiYSI6ImNsMWpieWJzOTExcjgzZXF1Mm1uYnI0ejUifQ.9pYx3prSS4bWr9w77OXpxA";
+console.log(mapboxgl.accessToken);
 const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: "map",
-      style: "mapbox://styles/athul0491/streets-v11",
-      center: [-99.29011, 39.39172],
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [78.9629, 20.5937],
       zoom: 3,
     });
   }, []);
